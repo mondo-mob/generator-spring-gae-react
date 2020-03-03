9e79f6652853e23d6086bcd325084cffb937b648
Matt Byrne <matt@3wks.com.au>
2020-03-04 09:43:54 +1100

    Rogue character fix

--- generators/app/templates/src/main/java/threewks/framework/usermanagement/repository/UserRepository.java
+++ generators/app/templates/src/main/java/threewks/framework/usermanagement/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package threewks.framework.usermanagement.repository;
 
-`import org.springframework.contrib.gae.objectify.repository.ObjectifyStringRepository;
+import org.springframework.contrib.gae.objectify.repository.ObjectifyStringRepository;
 import org.springframework.stereotype.Repository;
 import threewks.framework.usermanagement.Role;
 import threewks.framework.usermanagement.model.User;
