b5b0028882bec81898ce4bc30d9dcb69bfeb4ebe
Matt Byrne <matt@3wks.com.au>
2020-03-03 16:45:28 +1100

    Fix broken jest test due to history object needing to be supplied

--- generators/app/templates/src/test/javascript/reducers/index.test.js
+++ generators/app/templates/src/test/javascript/reducers/index.test.js
@@ -2,7 +2,7 @@ import rootReducer from '../../../main/client/javascript/reducers';
 
 describe('Root reducer', () => {
   test('returns form in state', () => {
-    const state = rootReducer(undefined, {});
+    const state = rootReducer({})(undefined, {});
 
     expect(state.form).toBeDefined();
   });
